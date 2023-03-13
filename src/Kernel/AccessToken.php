2c8f9be9d16a0d126d9b1f415a7ffaa4e0486350
Nick <i@httpd.cc>
2023-03-13 16:59:55 +0800

    fixbug

--- src/Kernel/AccessToken.php
+++ src/Kernel/AccessToken.php
@@ -6,7 +6,7 @@ class AccessToken extends \Cblink\Service\Foundation\AccessToken
 {
     public function getToken()
     {
-        return $this->app['config']->get('api_token');
+        return $this->app['config']->get('api_key');
     }
 
 }
